f44a1be6c8583f77c3b45087e9f683d1f70b546e
宝 小 <1392460179@qq.com>
2023-08-02 15:06:17 +0800

    新建

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div> <!--class="app-container home"-->
+
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {}
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, '_blank')
+    }
+  }
+}
+</script>
+
+
+
